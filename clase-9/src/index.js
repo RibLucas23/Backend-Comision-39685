10b0dde51c3360f3725cc86d8a3b423e6e4a8b8a
lucas ribeiro <rds.lucas23@gmail.com>
2023-02-13 16:09:04 -0300

    agregue status200

--- clase-9/src/index.js
+++ clase-9/src/index.js
@@ -1,21 +1,25 @@
+/* ---------------------- Modulos ----------------------*/
 import express from 'express'
 import routerProduct from './routes/products.routes.js'
 import routerCart from './routes/cart.routes.js'
 const app = express() //app es igual a la ejecucion de express
 const PORT = 8080
 
-//Middlewares
+/* ---------------------- Middlewares---------------------- */
 app.use(express.urlencoded({ extended: true })) //Permite realizar consultas en la URL (req.query)
 
-//Routes
-
+/* ---------------------- Routes---------------------- */
+// home
 app.get('/', (req, res) => {
     res.send("Servidor iniciado ")
 })
+// Routers
 app.use('/api/products', routerProduct)
 app.use('/api/cart', routerCart)
 
 
+
+/* --- Inicia server ---*/
 app.listen(PORT, () => {
     console.log(`Server on port http://localhost:${PORT}`)
 })
